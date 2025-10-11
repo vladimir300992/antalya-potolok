9295722018805ff7548626fe774446ee1fe3ed96
vladimir <vladimir.timoshenko7@gmail.com>
2025-10-11 14:39:40 +0300

    Fix faq

--- js/script.js
+++ js/script.js
@@ -188,7 +188,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 requestAnimationFrame(() => {
                     requestAnimationFrame(() => {
-                        const expandedHeight = answer.scrollHeight+10;
+                        const expandedHeight = answer.scrollHeight;
                         answer.style.setProperty('--faq-answer-max-height', `${expandedHeight}px`);
                     });
                 });
