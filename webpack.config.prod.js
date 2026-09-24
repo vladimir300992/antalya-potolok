b40ff82cfc8fab3bba9b69d8a0eb453981a2c93e
vladimir300992 <71935974+vladimir300992@users.noreply.github.com>
2026-09-25 00:04:09 +0300

    Redesign homepage with portfolio, reviews, pricing and local SEO

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,25 +1,36 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
+    new HtmlWebpackPlugin({ template: "./index.html", inject: false }),
     new CopyPlugin({
       patterns: [
-        { from: 'img', to: 'img' },
-        { from: 'css', to: 'css' },
-        { from: 'js/vendor', to: 'js/vendor' },
-        { from: 'icon.svg', to: 'icon.svg' },
-        { from: 'favicon.ico', to: 'favicon.ico' },
-        { from: 'robots.txt', to: 'robots.txt' },
-        { from: 'icon.png', to: 'icon.png' },
-        { from: '404.html', to: '404.html' },
-        { from: 'site.webmanifest', to: 'site.webmanifest' },
+        {
+          from: "*.html",
+          to: "[name][ext]",
+          globOptions: { ignore: ["**/index.html"] },
+        },
+        { from: "img", to: "img" },
+        { from: "css", to: "css" },
+        {
+          from: "js",
+          to: "js",
+          globOptions: { ignore: ["**/app.js", "**/AOS.js"] },
+        },
+        { from: "partials", to: "partials" },
+        ...[
+          "icon.svg",
+          "favicon.ico",
+          "robots.txt",
+          "sitemap.xml",
+          "icon.png",
+          "site.webmanifest",
+          "CNAME",
+        ].map((file) => ({ from: file, to: file })),
       ],
     }),
   ],
